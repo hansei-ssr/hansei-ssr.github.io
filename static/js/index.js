4c7e53dddcad59a07a32206e5aa7e52a9ac8d41f
Suhyun An <movptr06@outlook.kr>
2024-03-03 16:41:24 +0900

    Update: Navigation bar (remove position: fixed)

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,15 @@
+var state = "white"
+
 function show() {
     document.getElementById("load").remove();
 }
 
 function main() {
     show();
+
+    window.addEventListener("hashchange", (event) => {
+        history.replaceState({}, document.title, window.location.href.split('#')[0]);
+    });
 }
 
 window.onload = main;
